44c08b23bb90818d16b756abde883f99be3fb155
4MaticAirlines <9509119911s@mail.ru>
2025-05-21 01:48:09 +0300

    gg

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['molodtsov.sytes.net', 'molodtsov1.sytes.net']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'molodtsov.sytes.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
