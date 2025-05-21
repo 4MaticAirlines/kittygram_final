f4e8427fddcb77abff854c867d10f7def5f4a535
4MaticAirlines <9509119911s@mail.ru>
2025-05-21 21:23:24 +0300

    gg

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'molodtsov.sytes.net']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'molodtsov1.sytes.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -94,8 +94,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
